155e6de12cbb6bfc2b3766629b3ff96791f6848b
marina <marina-dominguez@alumni.nmu.edu>
2022-07-11 15:34:51 -0400

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let actualTemp = 77;
 let desireTemp = 80;
 
-if(actualTemp < 80){
+if(actualTemp < desireTemp){
     console.log("Run heat");
-} else if (actualTemp === 80){
+} else if (actualTemp === desireTemp){
     console.log("Standby");
 } else {
     console.log("Run A/C");
